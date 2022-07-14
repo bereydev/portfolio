ef43283a5dc338bd80eb0fc4ed2bf53a071c1b37
Jonathan Bereyziat <bereyziat.developer@gmail.com>
2022-07-14 21:40:27 +0200

    New website with translations

--- src/components.d.ts
+++ src/components.d.ts
@@ -11,7 +11,6 @@ declare module 'vue' {
     CarbonSun: typeof import('~icons/carbon/sun')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     FooterSection: typeof import('./components/FooterSection.vue')['default']
-    FooterSections: typeof import('./components/FooterSections.vue')['default']
     NavBar: typeof import('./components/NavBar.vue')['default']
     PricingSection: typeof import('./components/PricingSection.vue')['default']
     README: typeof import('./components/README.md')['default']
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,7 +23,7 @@ const { t } = useI18n()
   <NavBar />
 
   <!-- Section 2 -->
-  <section class="px-2 py-32 bg-white md:px-0">
+  <section class="px-2 py-10 md:py-32 bg-white md:px-0">
     <div class="container items-center max-w-6xl px-8 mx-auto xl:px-5">
       <div class="flex flex-wrap items-center sm:-mx-3">
         <div class="w-full md:w-1/2 md:px-3">
@@ -33,24 +33,24 @@ const { t } = useI18n()
             <h1
               class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl xl:text-6xl"
             >
-              <span class="block xl:inline">Tell us what you need and</span>
-              <span class="block text-indigo-600 xl:inline"> watch your idea come to life </span>
+              <span class="block xl:inline">{{ t('hero.title_1') }}<p /></span>
+              <span class="block gradient-title xl:inline">{{ t('hero.title_2') }}</span>
             </h1>
-            <p class="mx-auto text-base text-gray-500 sm:max-w-md lg:text-xl md:max-w-3xl">
-              With our agile team of developers, we help you to develop your projects with the most advanced tech and fist-class business advices.
+            <p class="mx-auto text-base text-2xl text-gray-500 sm:max-w-md md:max-w-3xl">
+              {{ t('hero.desc') }}
             </p>
             <div class="relative flex flex-col sm:flex-row sm:space-x-4">
               <a
-                href="https://calendly.com/bereydev"
-                class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-indigo-600 rounded-full sm:mb-0 hover:bg-indigo-700 sm:w-auto"
+                href="https://calendly.com/bereydev" target="_blank"
+                class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-purple-500 rounded-full sm:mb-0 hover:bg-purple-700 sm:w-auto"
               >
-                Let's talk
+                {{ t('button.talk') }}
               </a>
               <a
                 href="mailto:contact@bereyziat.dev"
-                class="flex items-center px-6 py-3 text-gray-500 bg-gray-100 rounded-full hover:bg-gray-200 hover:text-gray-600"
+                class="flex items-center px-6 py-3 text-lg text-gray-500 bg-gray-100 rounded-full hover:bg-gray-200 hover:text-gray-600"
               >
-                Send us an email
+                {{ t('button.send_mail') }}
               </a>
             </div>
           </div>
@@ -69,14 +69,14 @@ const { t } = useI18n()
   <!-- Section 3 -->
   <section class="w-full bg-white pt-7 pb-7 md:pt-20 md:pb-24">
     <div
-      class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16"
+      class="box-border flex flex-col items-center content-center mb-20 sm:mb-40 px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16"
     >
       <!-- Image -->
       <div
         class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10"
       >
         <img
-          src="https://cdn.devdojo.com/images/december2020/productivity.png"
+          src="/brainstorming.svg"
           class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "
         >
       </div>
@@ -84,21 +84,21 @@ const { t } = useI18n()
       <!-- Content -->
       <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
         <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300 lg:text-5xl md:text-4xl">
-          Brainstorm <br> with our engineers
+          {{ t('brainstorm.title_1') }} <br> {{ t('brainstorm.title_2') }}
         </h2>
-        <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-2xl">
-          Let's build together a strategic approach to your project in a creative atmosphere. <br>Our young team makes cool look easy.
+        <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 text-2xl border-gray-300 sm:pr-12 xl:pr-32">
+          {{ t('brainstorm.desc_1') }} <br>{{ t('brainstorm.desc_2') }}
         </p>
       </div>
       <!-- End  Content -->
     </div>
     <div
-      class="box-border flex flex-col items-center content-center px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16"
+      class="box-border flex flex-col items-center content-center mb-20 sm:mb-40 px-8 mx-auto mt-2 leading-6 text-black border-0 border-gray-300 border-solid md:mt-20 xl:mt-0 md:flex-row max-w-7xl lg:px-16"
     >
       <!-- Content -->
       <div class="box-border w-full text-black border-solid md:w-1/2 md:pl-6 xl:pl-32">
         <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300 lg:text-5xl md:text-4xl">
-          Design a product plan
+          {{ t('plan.title') }}
         </h2>
         <ul class="p-0 m-0 pt-4 leading-6 border-0 border-gray-300">
           <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
@@ -106,28 +106,28 @@ const { t } = useI18n()
               class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-purple-500 rounded-full"
             ><span
               class="text-sm font-bold"
-            >✓</span></span> <span class="text-2xl">We help you decide what tech would fit the best your needs</span>
+            >✓</span></span> <span class="text-2xl">{{ t('plan.bullet_1') }}</span>
           </li>
           <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
             <span
               class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-purple-500 rounded-full"
             ><span
               class="text-sm font-bold"
-            >✓</span></span> <span class="text-2xl">We define the steps to succeed with your idea</span>
+            >✓</span></span> <span class="text-2xl">{{ t('plan.bullet_2') }}</span>
           </li>
           <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
             <span
               class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-purple-500 rounded-full"
             ><span
               class="text-sm font-bold"
-            >✓</span></span> <span class="text-2xl">We define together a timeframe and a budget</span>
+            >✓</span></span> <span class="text-2xl">{{ t('plan.bullet_3') }}</span>
           </li>
           <li class="box-border relative py-1 pl-0 text-left text-gray-500 border-solid">
             <span
               class="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-purple-500 rounded-full"
             ><span
               class="text-sm font-bold"
-            >✓</span></span> <span class="text-2xl">We provide you with a free mock-up preview</span>
+            >✓</span></span> <span class="text-2xl">{{ t('plan.bullet_4') }}</span>
           </li>
         </ul>
       </div>
@@ -135,20 +135,20 @@ const { t } = useI18n()
 
       <!-- Image -->
       <div
-        class="box-border relative w-full max-w-md px-4 mt-10 mb-4 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2"
+        class="box-border relative w-full max-w-md px-4 mt-10 mb-20 sm:mb-40 text-center bg-no-repeat bg-contain border-solid md:mt-0 md:max-w-none lg:mb-0 md:w-1/2"
       >
-        <img src="https://cdn.devdojo.com/images/december2020/settings.png" class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32">
+        <img src="/planning.svg" class="pl-4 sm:pr-10 xl:pl-10 lg:pr-32">
       </div>
     </div>
     <div
-      class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16"
+      class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 mb-4 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16"
     >
       <!-- Image -->
       <div
         class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center bg-no-repeat bg-contain border-solid md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10"
       >
         <img
-          src="https://cdn.devdojo.com/images/december2020/productivity.png"
+          src="/launching.svg"
           class="p-2 pl-6 pr-5 xl:pl-16 xl:pr-20 "
         >
       </div>
@@ -156,13 +156,24 @@ const { t } = useI18n()
       <!-- Content -->
       <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
         <h2 class="m-0 text-3xl font-semibold leading-tight border-0 border-gray-300 lg:text-5xl md:text-4xl">
-          Make it happen
+          {{ t('launch.title') }}
         </h2>
-        <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-2xl">
-          Build an atmosphere that creates productivity in your organization and your company culture.
+        <p class="pt-4 pb-8 m-0 leading-7 text-gray-700 border-0 text-2xl border-gray-300 sm:pr-12 xl:pr-32">
+          {{ t('launch.desc_1') }}<br>
+          {{ t('launch.desc_2') }}
         </p>
       </div>
       <!-- End  Content -->
+    </div>
+  </section>
+  <section class="bg-white">
+    <div class="container flex flex-row justify-center items-center max-w-6xl px-8 mx-auto xl:px-5">
+      <a
+        href="https://calendly.com/bereydev" target="_blank"
+        class="flex items-center w-full px-6 py-3 mb-3 text-lg text-white bg-purple-500 rounded-full sm:mb-0 hover:bg-purple-700 sm:w-auto"
+      >
+        {{ t('button.book_meeting') }}
+      </a>
     </div>
   </section>
 
